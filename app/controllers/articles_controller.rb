1442994fef4926188ba6ee1a1401760248d9faf0
Maelle Rudner Mercier <mazz.r.m@gmail.com>
2023-10-14 16:29:19 -0400

    starting

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,53 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Articles.all
+  end
+
+  def show
+    @article = Aritcle.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article), notice: "Added to the article list!"
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    puts "Submitted parameters: #{params.inspect}"
+    if @article.update(article_params)
+      redirect_to article_path(@article), notice: "Article edited!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to article_path, status: :see_other, notice: "Article deleted!"
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
+
+
+end
